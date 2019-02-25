c460e00d1842173f5b2c639898e518ed19d5f915
waiss67 <baqui.waiss@gmail.com>
2019-02-25 15:39:44 +0100

    update for circularmenu

--- src/GraphManagerButton/SpinalLinkerButton.js
+++ src/GraphManagerButton/SpinalLinkerButton.js
@@ -1,36 +1,60 @@
 import {
   SpinalContextApp,
-  spinalContextMenuService
-} from "spinal-env-viewer-context-menu-service";
-import { spinalPanelManagerService } from "spinal-env-viewer-panel-manager-service";
-import {SPINAL_RELATION_PTR_LST_TYPE} from 'spinal-env-viewer-graph-service'
+  spinalContextMenuService,
+} from 'spinal-env-viewer-context-menu-service';
+import {
+  spinalPanelManagerService,
+} from 'spinal-env-viewer-panel-manager-service';
+import {
+  SpinalGraphService,
+  SPINAL_RELATION_PTR_LST_TYPE,
+} from 'spinal-env-viewer-graph-service';
+import SpinalNode from 'spinal-model-graph/build/Nodes/SpinalNode';
 
 export default class SpinalLinkerButton extends SpinalContextApp {
-
   constructor() {
-    super( "SpinalLinker", "Open SpinalLinker", {
-      icon: "link",
-      icon_type: "in",
-      backgroundColor: "#ffebf8",
-      fontColor: "#365bab"
-    } );
+    super('SpinalLinker', 'Open SpinalLinker', {
+      icon: 'link',
+      icon_type: 'in',
+      backgroundColor: '#ffebf8',
+      fontColor: '#365bab',
+    });
   }
 
-  isShown( option ) {
-    if (option.hasOwnProperty( 'selectedNode' ) && option.selectedNode.type.get() === 'BIMObject') {
-      return Promise.resolve( true );
+  isShown(option) {
+    if (option.selectedNode instanceof SpinalNode) {
+      return Promise.resolve(true);
+    } else {
+      if (
+        option.hasOwnProperty('selectedNode') &&
+        option.selectedNode.type.get() === 'BIMObject'
+      ) {
+        return Promise.resolve(true);
+      }
+
+      return Promise.resolve(true);
     }
-
-    return Promise.resolve( true );
   }
 
-  action( option ) {
-    const param = { relationName: "hasEndPoint", relationType: SPINAL_RELATION_PTR_LST_TYPE, selectedNode: option.selectedNode };
-    spinalPanelManagerService.openPanel( "plugin-spinal-linker", param );
+  action(option) {
+    let selectedNode = option.selectedNode;
+    if (option.selectedNode instanceof SpinalNode) {
+      selectedNode = SpinalGraphService.getInfo(option.selectedNode.getId());
+    }
+    const param = {
+      relationName: 'hasEndPoint',
+      relationType: SPINAL_RELATION_PTR_LST_TYPE,
+      selectedNode: selectedNode,
+    };
+    spinalPanelManagerService.openPanel('plugin-spinal-linker', param);
   }
-
 }
 
-const TopBarName = "GraphManagerSideBar";
+const TopBarName = 'GraphManagerSideBar';
+const circularMenuName = 'circularMenu';
 
-spinalContextMenuService.registerApp( TopBarName, new SpinalLinkerButton() );
+spinalContextMenuService.registerApp(TopBarName, new SpinalLinkerButton());
+spinalContextMenuService.registerApp(
+  circularMenuName,
+  new SpinalLinkerButton()
+);
